df7ff1e469debb68a0fbceacef031d27e01e7171
Charlie Dill <charlie_dill@Charlies-MacBook-Pro-2.local>
2017-10-17 22:46:13 -0500

    created several templates and handlers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
@@ -7,6 +7,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:lc101blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zp3B'
 
 class Blog(db.Model):
 
@@ -26,7 +27,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
 
-    def __init__(self, email, password, blogs):
+    def __init__(self, email, password):
         self.email= email
         self.password= password
 
@@ -39,28 +40,63 @@ class User(db.Model):
         return '<Body %r>' % self.body
 
 
-        def get_curent_titles():
-            return Title.query.all()
-
-        def get_current_blogs():
-            return Body.query.all()
 
    
 @app.route('/')
-def index():
-       
-    #blogs = Blog.query.all()
-        #new_blog = Blog(title, body)
-        
-         #new_body = Blog(body)
-        #db.session.add(new_blog)
-        #db.session.commit()  
-           
-    blogs = Blog.query.all()
-    return render_template('blog.html', blogs=blogs)
+def index():     
+    users = User.query.all()
+    return render_template('index.html', users=users)
     
-    
-    
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect ('/login')
+
+
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email'] = email
+            flash("logged in")
+            return redirect ('/')
+        else:
+            flash('User password incorrect, or user does not exist', 'error')
+    return render_template('login.html')
+ 
+ 
+@app.route('/signup', methods = ['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+        #TODO - vlaidate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+
+            return redirect('/')
+        else:
+            #TODO - use better response messaging
+            return '<h1>Duplicate user</h1>'
+
+    return render_template('signup.html')   
+
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')   
 
 @app.route('/post', methods=['POST', 'GET'])
 def display_post():
@@ -72,6 +108,15 @@ def display_post():
             return render_template('post.html', blog=blog)
 
 
+@app.route('/singleuser', methods=['POST', 'GET'])
+def display_user_posts():
+    if request.method == 'GET':
+        user_id = request.args.get('User.id')
+        user = User.query.filter_by(id=user_id).first()
+        if user_id:
+            return render_template('singleuser', user=user)
+
+    
 
     
 @app.route("/addpost", methods=['POST', 'GET'])
